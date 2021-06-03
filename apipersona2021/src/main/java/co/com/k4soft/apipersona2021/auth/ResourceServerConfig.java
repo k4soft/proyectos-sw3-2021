ecc6013fee55c56249d2f10d3e18027a221383d3
k4soft <info@k4soft.com>
2021-06-02 22:05:07 -0500

    se actualizan rutas

--- apipersona2021/src/main/java/co/com/k4soft/apipersona2021/auth/ResourceServerConfig.java
+++ apipersona2021/src/main/java/co/com/k4soft/apipersona2021/auth/ResourceServerConfig.java
@@ -27,7 +27,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public CorsConfigurationSource getCorsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("http://localhost"));
+		config.setAllowedOrigins(Arrays.asList("http://localhost:8888"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
